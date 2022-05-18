2942c471740524595b8b0cb2972131a2b8feafaf
LiangTian <786027650@qq.com>
2022-05-18 12:26:35 +0800

    加入与蓝牙设备联动代码

--- Assets/Oculus/SampleFramework/Core/Video/Scripts/MoviePlayerSample.cs
+++ Assets/Oculus/SampleFramework/Core/Video/Scripts/MoviePlayerSample.cs
@@ -7,14 +7,16 @@ Copyright (c) Facebook Technologies, LLC and its affiliates. All rights reserved
 using UnityEngine;
 using System;
 using System.IO;
+using System.Threading;
+using UnityEngine.Video;
 
 public class MoviePlayerSample : MonoBehaviour
 {
-    private bool    videoPausedBeforeAppPause = false;
+    private bool videoPausedBeforeAppPause = false;
 
     private UnityEngine.Video.VideoPlayer videoPlayer = null;
-    private OVROverlay          overlay = null;
-    private Renderer            mediaRenderer = null;
+    private OVROverlay overlay = null;
+    private Renderer mediaRenderer = null;
 
     public bool IsPlaying { get; private set; }
     public long Duration { get; private set; }
@@ -32,8 +34,8 @@ public class MoviePlayerSample : MonoBehaviour
     public bool DisplayMono;
 
     // keep track of last state so we know when to update our display
-    VideoShape _LastShape = (VideoShape)(-1);
-    VideoStereo _LastStereo = (VideoStereo)(-1);
+    VideoShape _LastShape = (VideoShape) (-1);
+    VideoStereo _LastStereo = (VideoStereo) (-1);
     bool _LastDisplayMono = false;
 
     public enum VideoShape
@@ -74,7 +76,8 @@ public class MoviePlayerSample : MonoBehaviour
         // only can use external surface with native plugin
         overlay.isExternalSurface = NativeVideoPlayer.IsAvailable;
         // only mobile has Equirect shape
-        overlay.enabled = (overlay.currentOverlayShape != OVROverlay.OverlayShape.Equirect || Application.platform == RuntimePlatform.Android);
+        overlay.enabled = (overlay.currentOverlayShape != OVROverlay.OverlayShape.Equirect ||
+                           Application.platform == RuntimePlatform.Android);
 
 #if UNITY_EDITOR
         overlay.currentOverlayShape = OVROverlay.OverlayShape.Quad;
@@ -96,7 +99,7 @@ public class MoviePlayerSample : MonoBehaviour
             {
                 int w = NativeVideoPlayer.VideoWidth;
                 int h = NativeVideoPlayer.VideoHeight;
-                switch(NativeVideoPlayer.VideoStereoMode)
+                switch (NativeVideoPlayer.VideoStereoMode)
                 {
                     case NativeVideoPlayer.StereoMode.Mono:
                         Stereo = VideoStereo.Mono;
@@ -116,6 +119,7 @@ public class MoviePlayerSample : MonoBehaviour
                         {
                             Stereo = VideoStereo.TopBottom;
                         }
+
                         break;
                 }
             }
@@ -149,17 +153,17 @@ public class MoviePlayerSample : MonoBehaviour
             {
                 case VideoStereo.LeftRight:
                     // set source matrices for left/right
-                    sourceLeft  = new Rect(0.0f, 0.0f, 0.5f, 1.0f);
+                    sourceLeft = new Rect(0.0f, 0.0f, 0.5f, 1.0f);
                     sourceRight = new Rect(0.5f, 0.0f, 0.5f, 1.0f);
                     break;
                 case VideoStereo.TopBottom:
                     // set source matrices for top/bottom
-                    sourceLeft  = new Rect(0.0f, 0.5f, 1.0f, 0.5f);
+                    sourceLeft = new Rect(0.0f, 0.5f, 1.0f, 0.5f);
                     sourceRight = new Rect(0.0f, 0.0f, 1.0f, 0.5f);
                     break;
                 case VideoStereo.BottomTop:
                     // set source matrices for top/bottom
-                    sourceLeft  = new Rect(0.0f, 0.0f, 1.0f, 0.5f);
+                    sourceLeft = new Rect(0.0f, 0.0f, 1.0f, 0.5f);
                     sourceRight = new Rect(0.0f, 0.5f, 1.0f, 0.5f);
                     break;
             }
@@ -172,43 +176,23 @@ public class MoviePlayerSample : MonoBehaviour
         }
     }
 
-    private System.Collections.IEnumerator Start()
+    private void Start()
     {
-        if (mediaRenderer.material == null)
-        {
-            Debug.LogError("No material for movie surface");
-            yield break;
-        }
-
-        // wait 1 second to start (there is a bug in Unity where starting
-        // the video too soon will cause it to fail to load)
-        yield return new WaitForSeconds(1.0f);
-
-        if (!string.IsNullOrEmpty(MovieName))
-        {
-            if (IsLocalVideo(MovieName))
-            {
+        print("Start");
+        Loom.Initialize();
 #if UNITY_EDITOR
-                // in editor, just pull in the movie file from wherever it lives (to test without putting in streaming assets)
-                var guids = UnityEditor.AssetDatabase.FindAssets(Path.GetFileNameWithoutExtension(MovieName));
-
-                if (guids.Length > 0)
-                {
-                    string video = UnityEditor.AssetDatabase.GUIDToAssetPath(guids[0]);
-                    Play(video, null);
-                }
+        StartRecvDataTimer(true);
+        Play(MovieName);
 #else
-                Play(Application.streamingAssetsPath +"/" + MovieName, null);
+        jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        // 由安卓那边调用Play(MovieName)，因为那边要先做一些准备处理，比如解析埋点数据;
+        Play("MovieName");
 #endif
-            }
-            else
-            {
-                Play(MovieName, DrmLicenseUrl);
-            }
-        }
     }
 
-    public void Play(string moviePath, string drmLicencesUrl)
+
+    void Play(string moviePath, string drmLicencesUrl)
     {
         if (moviePath != string.Empty)
         {
@@ -258,6 +242,7 @@ public class MoviePlayerSample : MonoBehaviour
         {
             videoPlayer.Play();
         }
+
         IsPlaying = true;
     }
 
@@ -271,6 +256,7 @@ public class MoviePlayerSample : MonoBehaviour
         {
             videoPlayer.Pause();
         }
+
         IsPlaying = false;
     }
 
@@ -309,11 +295,12 @@ public class MoviePlayerSample : MonoBehaviour
                 mediaRenderer.material.SetVector("_SrcRectLeft", overlay.srcRectLeft.ToVector());
                 mediaRenderer.material.SetVector("_SrcRectRight", overlay.srcRectRight.ToVector());
             }
+
             IsPlaying = videoPlayer.isPlaying;
-            PlaybackPosition = (long)(videoPlayer.time * 1000L);
+            PlaybackPosition = (long) (videoPlayer.time * 1000L);
 
 #if UNITY_2019_1_OR_NEWER
-            Duration = (long)(videoPlayer.length * 1000L); 
+            Duration = (long) (videoPlayer.length * 1000L);
 #else
             Duration = videoPlayer.frameRate > 0 ? (long)(videoPlayer.frameCount / videoPlayer.frameRate * 1000L) : 0L;
 #endif
@@ -324,16 +311,16 @@ public class MoviePlayerSample : MonoBehaviour
             IsPlaying = NativeVideoPlayer.IsPlaying;
             PlaybackPosition = NativeVideoPlayer.PlaybackPosition;
             Duration = NativeVideoPlayer.Duration;
-            if (IsPlaying && (int)OVRManager.display.displayFrequency != 60)
+            if (IsPlaying && (int) OVRManager.display.displayFrequency != 60)
             {
                 OVRManager.display.displayFrequency = 60.0f;
             }
-            else if (!IsPlaying && (int)OVRManager.display.displayFrequency != 72)
+            else if (!IsPlaying && (int) OVRManager.display.displayFrequency != 72)
             {
                 OVRManager.display.displayFrequency = 72.0f;
             }
         }
-  }
+    }
 
     public void SetPlaybackSpeed(float speed)
     {
@@ -347,7 +334,7 @@ public class MoviePlayerSample : MonoBehaviour
         {
             videoPlayer.playbackSpeed = speed;
         }
-    }    
+    }
 
     public void Stop()
     {
@@ -363,10 +350,10 @@ public class MoviePlayerSample : MonoBehaviour
         IsPlaying = false;
     }
 
-  /// <summary>
-  /// Pauses video playback when the app loses or gains focus
-  /// </summary>
-  void OnApplicationPause(bool appWasPaused)
+    /// <summary>
+    /// Pauses video playback when the app loses or gains focus
+    /// </summary>
+    void OnApplicationPause(bool appWasPaused)
     {
         Debug.Log("OnApplicationPause: " + appWasPaused);
         if (appWasPaused)
@@ -387,4 +374,107 @@ public class MoviePlayerSample : MonoBehaviour
             }
         }
     }
+
+    #region 实现与蓝牙设备联动的代码
+
+    //通过包名获取java class
+    AndroidJavaClass jc;
+
+    //获取当前的activity
+    AndroidJavaObject jo;
+
+    //该方法给安卓调用
+    void ShowToast(string msg)
+    {
+        print("ShowToast from unity" + msg);
+        jo.Call("showToast", msg);
+    }
+
+    //定时器
+    void StartRecvDataTimer(bool autoFlag)
+    {
+        //实例化Timer类，设置间隔时间为100毫秒；
+        System.Timers.Timer t = new System.Timers.Timer(100);
+        t.Elapsed += new System.Timers.ElapsedEventHandler(ShowRecv);
+        t.AutoReset = autoFlag; //设置是执行一次（false）还是一直执行(true)；
+        t.Enabled = true; //是否执行System.Timers.Timer.Elapsed事件；
+    }
+
+    //到达时间的时候执行事件；
+    void ShowRecv(object source, System.Timers.ElapsedEventArgs e)
+    {
+        // print("ShowRecv");
+        // int i = Thread.CurrentThread.ManagedThreadId;
+        // print(i);
+        //异步在多线程下运行
+        //回到unity线程继续运行
+        //耗时操作执行完毕后，就可以用Loom的方法在Unity主线程中调用Text组件
+        Loom.QueueOnMainThread((param) =>
+        {
+            // int i2 = Thread.CurrentThread.ManagedThreadId;
+            // print(i2);
+#if UNITY_EDITOR
+            print("当前播放进度:" + GetTime());
+#else
+        print("当前播放进度:"+GetTime());
+        //调用安卓那边触发埋点的方法，并把当前播放进度传过去
+        jo.Call("showToast", "你好哇");
+#endif
+        }, null);
+    }
+
+    //获取当前播放进度，并转换为毫秒，这样的转换其实不够准确，但是能精确到100毫秒即可
+    public long GetTime()
+    {
+        return (long) (videoPlayer.time * 1000.0);
+    }
+
+    //在合适的时候调用播放,注意：如果是在start方法里调用最好延迟一秒，因为untiy有bug，但是我未发现，是这个库的官方提示要这么做
+    void Play(string moviePathOrUrl)
+    {
+        //在start里调用加上如下注释代码，并让start方法返回System.Collections.IEnumerator，这是库的官方提示要这么做
+        // if (mediaRenderer.material == null)
+        // {
+        //     Debug.LogError("No material for movie surface");
+        //     yield break;
+        // }
+        //
+        // // wait 1 second to start (there is a bug in Unity where starting
+        // // the video too soon will cause it to fail to load)
+        // yield return new WaitForSeconds(1.0f);
+        print("调用了梁天定义的Play");
+        MovieName = moviePathOrUrl;
+        if (!string.IsNullOrEmpty(MovieName))
+        {
+            if (IsLocalVideo(MovieName))
+            {
+#if UNITY_EDITOR
+                // in editor, just pull in the movie file from wherever it lives (to test without putting in streaming assets)
+                var guids = UnityEditor.AssetDatabase.FindAssets(Path.GetFileNameWithoutExtension(MovieName));
+
+                if (guids.Length > 0)
+                {
+                    string video = UnityEditor.AssetDatabase.GUIDToAssetPath(guids[0]);
+                    print("111111");
+                    Play(video, null);
+                }
+#else
+                 print("2222222");
+                Play(Application.streamingAssetsPath +"/" + MovieName, null);
+#endif
+            }
+            else
+            {
+                print("33333");
+                Play(MovieName, DrmLicenseUrl);
+            }
+        }
+        //播放视频后开启定时器，每100毫秒调用一次安卓那边触发埋点的方法，并把当前播放进度传过去，注:如果有视频播放成功回调方法，可以放到回调方法里执行
+#if UNITY_EDITOR
+#else
+      StartRecvDataTimer(true);
+#endif
+    }
+
+    #endregion
 }
